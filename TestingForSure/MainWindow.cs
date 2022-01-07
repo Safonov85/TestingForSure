e17623dd467a6963af9820bacde91521c2cd945c
Safonov85 <safonov1985@protonmail.com>
2022-01-07 20:16:49 +0100

    First commit

---
+++ TestingForSure/MainWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+
+    //Point noPoint;
+    //Random ran;
+    //Lul lal;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButtonMessageClicked(object sender, EventArgs e)
+    {
+        this.Title = "Ga-ga Buuuu";
+
+    }
+}
