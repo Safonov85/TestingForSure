597ac07cfec93287149e22d380c10523b628e53e
Safonov85 <safonov1985@protonmail.com>
2022-01-12 01:16:27 +0100

    Created MessageBox and small calc

--- TestingForSure/MainWindow.cs
+++ TestingForSure/MainWindow.cs
@@ -4,15 +4,7 @@ using Gtk;
 public partial class MainWindow : Gtk.Window
 {
 
-    //Point noPoint;
-    //Random ran;
-    //Lul lal;
-
-    public MainWindow() : base(Gtk.WindowType.Toplevel)
-    {
-        Build();
-        
-    }
+    public MainWindow() : base(Gtk.WindowType.Toplevel) => Build();
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
     {
@@ -22,7 +14,57 @@ public partial class MainWindow : Gtk.Window
 
     protected void OnButtonMessageClicked(object sender, EventArgs e)
     {
-        this.Title = "Ga-ga Buuuu";
+        this.Title = "New program 2022";
 
+        NewMessage();
+
+    }
+
+    void NewMessage()
+    {
+        MessageDialog messageBox = new MessageDialog(
+        this,
+        DialogFlags.DestroyWithParent,
+        MessageType.Info,
+        ButtonsType.Ok,
+        "MessageBox Works!");
+
+        messageBox.Run();
+        messageBox.Destroy();
+    }
+
+    protected void OnButtonCalculateSumClicked(object sender, EventArgs e)
+    {
+        CalculateNow();
+    }
+
+    void CalculateNow()
+    {
+
+        labelResult.Text = GetSumOfNumbers(comboboxArithmetic.ActiveText, float.Parse(spinbuttonNum1.Text), float.Parse(TextBoxSecondNum2.Text)).ToString();
+    }
+
+    float GetSumOfNumbers(string operation, float num1, float num2)
+    {
+        float result = 0f;
+
+        if(operation == "+")
+        {
+            result = num1 + num2;
+        }
+        else if(operation == "-")
+        {
+            result = num1 - num2;
+        }
+        else if (operation == "*")
+        {
+            result = num1 * num2;
+        }
+        else if (operation == "/")
+        {
+            result = num1 / num2;
+        }
+
+        return result;
     }
 }
--- TestingForSure/gtk-gui/MainWindow.cs
+++ TestingForSure/gtk-gui/MainWindow.cs
@@ -7,7 +7,15 @@ public partial class MainWindow
 
 	private global::Gtk.Button ButtonMessage;
 
-	private global::Gtk.SpinButton spinbutton1;
+	private global::Gtk.SpinButton spinbuttonNum1;
+
+	private global::Gtk.ComboBox comboboxArithmetic;
+
+	private global::Gtk.Entry TextBoxSecondNum2;
+
+	private global::Gtk.Button ButtonCalculateSum;
+
+	private global::Gtk.Label labelResult;
 
 	protected virtual void Build()
 	{
@@ -29,20 +37,60 @@ public partial class MainWindow
 		this.ButtonMessage.Label = global::Mono.Unix.Catalog.GetString("      Try     ");
 		this.fixed1.Add(this.ButtonMessage);
 		global::Gtk.Fixed.FixedChild w1 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.ButtonMessage]));
-		w1.X = 82;
-		w1.Y = 66;
+		w1.X = 66;
+		w1.Y = 57;
 		// Container child fixed1.Gtk.Fixed+FixedChild
-		this.spinbutton1 = new global::Gtk.SpinButton(0D, 100D, 1D);
-		this.spinbutton1.CanFocus = true;
-		this.spinbutton1.Name = "spinbutton1";
-		this.spinbutton1.Adjustment.PageIncrement = 10D;
-		this.spinbutton1.ClimbRate = 1D;
-		this.spinbutton1.Numeric = true;
-		this.spinbutton1.Value = 3D;
-		this.fixed1.Add(this.spinbutton1);
-		global::Gtk.Fixed.FixedChild w2 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.spinbutton1]));
-		w2.X = 81;
-		w2.Y = 150;
+		this.spinbuttonNum1 = new global::Gtk.SpinButton(0D, 100D, 1D);
+		this.spinbuttonNum1.CanFocus = true;
+		this.spinbuttonNum1.Name = "spinbuttonNum1";
+		this.spinbuttonNum1.Adjustment.PageIncrement = 10D;
+		this.spinbuttonNum1.ClimbRate = 1D;
+		this.spinbuttonNum1.Numeric = true;
+		this.spinbuttonNum1.Value = 1D;
+		this.fixed1.Add(this.spinbuttonNum1);
+		global::Gtk.Fixed.FixedChild w2 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.spinbuttonNum1]));
+		w2.X = 85;
+		w2.Y = 132;
+		// Container child fixed1.Gtk.Fixed+FixedChild
+		this.comboboxArithmetic = global::Gtk.ComboBox.NewText();
+		this.comboboxArithmetic.AppendText(global::Mono.Unix.Catalog.GetString("+"));
+		this.comboboxArithmetic.AppendText(global::Mono.Unix.Catalog.GetString("-"));
+		this.comboboxArithmetic.AppendText(global::Mono.Unix.Catalog.GetString("*"));
+		this.comboboxArithmetic.AppendText(global::Mono.Unix.Catalog.GetString("/"));
+		this.comboboxArithmetic.Name = "comboboxArithmetic";
+		this.fixed1.Add(this.comboboxArithmetic);
+		global::Gtk.Fixed.FixedChild w3 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.comboboxArithmetic]));
+		w3.X = 166;
+		w3.Y = 129;
+		// Container child fixed1.Gtk.Fixed+FixedChild
+		this.TextBoxSecondNum2 = new global::Gtk.Entry();
+		this.TextBoxSecondNum2.WidthRequest = 93;
+		this.TextBoxSecondNum2.CanFocus = true;
+		this.TextBoxSecondNum2.Name = "TextBoxSecondNum2";
+		this.TextBoxSecondNum2.IsEditable = true;
+		this.TextBoxSecondNum2.InvisibleChar = '•';
+		this.fixed1.Add(this.TextBoxSecondNum2);
+		global::Gtk.Fixed.FixedChild w4 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.TextBoxSecondNum2]));
+		w4.X = 255;
+		w4.Y = 131;
+		// Container child fixed1.Gtk.Fixed+FixedChild
+		this.ButtonCalculateSum = new global::Gtk.Button();
+		this.ButtonCalculateSum.CanFocus = true;
+		this.ButtonCalculateSum.Name = "ButtonCalculateSum";
+		this.ButtonCalculateSum.UseUnderline = true;
+		this.ButtonCalculateSum.Label = global::Mono.Unix.Catalog.GetString("Calculate!");
+		this.fixed1.Add(this.ButtonCalculateSum);
+		global::Gtk.Fixed.FixedChild w5 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.ButtonCalculateSum]));
+		w5.X = 380;
+		w5.Y = 128;
+		// Container child fixed1.Gtk.Fixed+FixedChild
+		this.labelResult = new global::Gtk.Label();
+		this.labelResult.Name = "labelResult";
+		this.labelResult.LabelProp = global::Mono.Unix.Catalog.GetString("label1");
+		this.fixed1.Add(this.labelResult);
+		global::Gtk.Fixed.FixedChild w6 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.labelResult]));
+		w6.X = 542;
+		w6.Y = 132;
 		this.Add(this.fixed1);
 		if ((this.Child != null))
 		{
@@ -53,5 +101,6 @@ public partial class MainWindow
 		this.Show();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler(this.OnDeleteEvent);
 		this.ButtonMessage.Clicked += new global::System.EventHandler(this.OnButtonMessageClicked);
+		this.ButtonCalculateSum.Clicked += new global::System.EventHandler(this.OnButtonCalculateSumClicked);
 	}
 }
